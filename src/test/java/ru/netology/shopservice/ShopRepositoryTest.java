d65c2b808ab0f19fb70a92ce1a00f4752575d1d3
Anna Shcherbakova <ShcherbakovaAnna90@yandex.ru>
2023-12-18 18:30:59 +0600

    Shop repository implementation

---
+++ src/test/java/ru/netology/shopservice/ShopRepositoryTest.java
@@ -0,0 +1,45 @@
+package ru.netology.shopservice;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.netology.shopservice.Product;
+
+public class ShopRepositoryTest {
+
+    Product book = new Product(1, "book", 123);
+    Product laptop = new Product(2, "laptop", 456);
+    Product cellphone = new Product(3, "cellphone", 789);
+    ShopRepository shopRepository = new ShopRepository();
+
+    @Test
+    public void shouldFindById() {
+       shopRepository.add(book);
+
+       Product expected = book;
+       Product actual = shopRepository.findById(1);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldRemoveById() {
+        shopRepository.add(book);
+        shopRepository.add(laptop);
+
+        shopRepository.remove(2);
+        Product[] expected = {book};
+        Product[] actual = shopRepository.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldTrowException() {
+        shopRepository.add(book);
+
+        Assertions.assertThrows(NotFoundException.class, () -> {
+            shopRepository.remove(2);
+        });
+    }
+
+}
